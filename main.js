6432724743519b2b390289fbea0e30f5337f658c
Carlos Huerta <carlos.h.huertag@gmail.com>
2021-03-03 14:10:06 -0300

    process port

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug')
-app.listen(3000)
+app.set("view engine", "pug");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:5000");
+});
 
-app.use('/assets', express.static(__dirname + '/assets/'))
+app.use("/assets", express.static(__dirname + "/assets/"));
 
-
-
-app.get('/', (req,res)=>{
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
